2f9cea1a96b778a41ba58139ccb0a35bf43412bf
VK911 TECH <gbolahanraifu1@gmail.com>
2026-05-09 21:46:20 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,8 @@
-export default async function handler(req, res) {
-  try {
-    const mod = await import("../build/server/index.js");
+import { createRequestListener } from "@react-router/node";
+import * as build from "../build/server/index.js";
 
-    const info = {
-      moduleKeys: Object.keys(mod),
-      hasDefault: !!mod.default,
-      defaultType: typeof mod.default,
-      defaultKeys: mod.default ? Object.keys(mod.default) : [],
-      hasRoutes: !!mod.routes,
-      routesType: typeof mod.routes,
-      defaultHasRoutes: !!(mod.default?.routes),
-    };
+const handler = createRequestListener(build, {
+  mode: "production",
+});
 
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(info, null, 2));
-  } catch (err) {
-    res.statusCode = 500;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Error: " + err.message + "\n" + err.stack);
-  }
-}
+export default handler;
