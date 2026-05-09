e91b750aa55502fd95cc9cb14cce67b1c7e4cc19
VK911 TECH <gbolahanraifu1@gmail.com>
2026-05-09 18:06:59 +0100

    postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
+// Tailwind v4 with @tailwindcss/vite does NOT need PostCSS config.
+// CSS is processed directly by the Vite plugin.
 export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  plugins: {}
 };
