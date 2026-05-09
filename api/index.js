b199f6c8c1845b79a5778903d7e5eeb849083b69
VK911 TECH <gbolahanraifu1@gmail.com>
2026-05-09 21:21:06 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import { createRequestListener } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-const handler = createRequestListener(build, {
+console.log("Build exports:", Object.keys(build));
+console.log("Build.default:", typeof build.default);
+console.log("Routes:", build.routes);
+console.log("Entry:", build.entry);
+
+// Try default export if named exports don't have routes
+const serverBuild = build.routes ? build : build.default;
+
+const handler = createRequestListener(serverBuild, {
   mode: process.env.NODE_ENV || "production",
 });
 
