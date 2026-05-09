f4746f52937fafbdf3c8360effb094dae300f8fb
VK911 TECH <gbolahanraifu1@gmail.com>
2026-05-09 20:48:35 +0100

    index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,28 @@
-import * as ReactRouterNode from "@react-router/node";
+export default async function handler(req, res) {
+  try {
+    const nodeModule = await import("@react-router/node");
+    console.log("@react-router/node exports:", Object.keys(nodeModule));
 
-const handler = ReactRouterNode.createRequestHandler(
-  () => import("../build/server/index.js"),
-  process.env.NODE_ENV || "production"
-);
+    const createRequestHandler = nodeModule.createRequestHandler;
 
-export default handler;
+    if (!createRequestHandler) {
+      res.statusCode = 500;
+      res.end(
+        "createRequestHandler not found. Available exports: " +
+          Object.keys(nodeModule).join(", ")
+      );
+      return;
+    }
+
+    const handle = createRequestHandler(
+      () => import("../build/server/index.js"),
+      process.env.NODE_ENV || "production"
+    );
+
+    return handle(req, res);
+  } catch (err) {
+    console.error("Handler error:", err);
+    res.statusCode = 500;
+    res.end(err.message);
+  }
+}
