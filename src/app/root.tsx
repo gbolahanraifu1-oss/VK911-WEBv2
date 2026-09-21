8e3e6b3a4813ff8c8fda8c2d129c6ff22b362250
ᴍᴀᴅᴀʀᴀ x-ᴍᴅ | ɪɴᴄ. <gbolahanraifu1@gmail.com>
2026-09-21 02:26:27 +0100

    Update root.tsx

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -24,11 +24,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <title>ᴍᴀᴅᴀʀᴀ x-ᴍᴅ</title>
-        <meta name="google-adsense-account" content="ca-pub-5934268333067094" />
-        <meta name="monetag" content="a8c554baae03d4261726e149d47ff2dc">
-        <script src="https://quge5.com/88/tag.min.js" data-zone="278684" async data-cfasync="false"></script>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5934268333067094" crossOrigin="anonymous"></script>
-        <Links />
+       <meta name="monetag" content="a8c554baae03d4261726e149d47ff2dc" />
+
+<script
+  src="https://quge5.com/88/tag.min.js"
+  data-zone="278684"
+  async
+  data-cfasync="false"
+/>
+
+<script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5934268333067094"
+  crossOrigin="anonymous"
+/>        <Links />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500;600&display=swap"
           rel="stylesheet"
